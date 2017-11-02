0fd1664007b31011ba3af067f800696a9ae772ab
Carmen Perezchica <crperezchica@gmail.com>
2017-11-02 15:22:00 -0700

    Form created and operating with current data

--- sales.js
+++ sales.js
@@ -1,17 +1,8 @@
 'user strict';
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm','2pm','3pm','4pm','5pm','6pm', '7pm'];
-const locations = ['Portland Airport', 'Pioneer Square', 'Powells', 'St Johns', 'Waterfront'];
 
-
-/*
-Store
-    - name
-    - location
-    - cookieType
-    - averageCookieSales
-*/
-
+const locations = ('Portland Airport', 'Pioneer Square', 'Powells', 'St Johns', 'Waterfront');
 //Class created via a constructor function. 
 function Store (name, min, max, avg) {
     this.name=name; //created method for name by using this.
@@ -19,6 +10,7 @@ function Store (name, min, max, avg) {
     this.max=max;
     this.avg=avg;
     this.hourlySales();
+
 }
 
 //since we created a constructor function and an instance of a store. I had to create a prototype of the store for hourly sales and the function created to run/calculate 
@@ -37,8 +29,16 @@ Store.prototype.hourlySales = function (){
     }
    this.salesByHour = salesByHour;
 }
+function getRandomInInclusive(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1 )) + min;
+}
 
-//created a function with the prototype render ? to create table with store name and hourly sales data 
+
+
+
+ //created a function with the prototype render ? to create table with store name and hourly sales data 
 Store.prototype.render = function (){
     const tbody = document.getElementById ('table-body');
     const tr = document.createElement('TR');
@@ -48,14 +48,13 @@ Store.prototype.render = function (){
   
     tbody.appendChild(tr)
 
-    for (let i =0; i < hours.length; i++) {
+    for (let i =0; i < hours.length; i++){
         const td = document.createElement('TD');
         td.textContent = this.salesByHour[i].cookiesSold;
         tr.appendChild(td);
     }
+
 }
-
-
 
 
 //constructor function is used to populate each of the stores with the parameters being pased under new Store in between ();
@@ -65,7 +64,7 @@ const powells = new Store ('Powells', 11, 38, 3.7);
 const stJohns = new Store ('St Johns', 20, 38, 2.3);
 const waterfront = new Store ('Waterfront', 2, 16, 4.6);
 
-console.log(pdx);
+console.log(pdx)
 
 pdx.render();
 pioneer.render();
@@ -75,15 +74,6 @@ waterfront.render();
 
 renderHoursRow();
 
-
-
-
-function getRandomInInclusive(min,max){
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min +1 )) + min;
-}
-
 function renderHoursRow (){
     const thead = document.getElementById('table-head');
     const tr = document.createElement('TR');
@@ -91,10 +81,35 @@ function renderHoursRow (){
     const emptyTh = document.createElement('TH');
     tr.appendChild(emptyTh);
 
-    for (let i=0; i < hours.length; i++) {
+    for (let i=0; i<hours.length; i++) {
         const hour = hours[i];
         const th = document.createElement('TH');
         th.textContent = hours[i];
         tr.appendChild(th);
     }
 }
+
+
+const stores = document.getElementById('stores');
+
+stores.appendChild(pdx.render());
+stores.appendChild(pioneer.render());
+stores.appendChild(powells.render());
+stores.appendChild(stJohns.render());
+stores.appendChild(waterfront.render());
+
+const form = document.getElementById('new-stores');
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log('the form heard a submit event!');
+
+    const storename = document.getElementById('storename').value;
+    const min = document.getElementById('min').value;
+    const max = document.getElementById('max').value;
+    const avg = document.getElementById('avg').value;
+
+    const newStore = new Store (storename, min,max,avg);
+    stores.appendChild(newStore.render());
+});
